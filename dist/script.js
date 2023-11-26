ffde6d463534be670f7ea3d23a0e7ce1a7412397
rumikadms <rumikadamayantims@gmail.com>
2023-11-27 03:57:55 +0700

    update

--- dist/script.js
+++ dist/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixNav = header.offsetTop;
+    const ToTop = document.querySelector('#toTop');
 
     if(window.pageYOffset > fixNav) {
         header.classList.add('navbar-fixed');
+        ToTop.classList.remove('hidden');
+        ToTop.classList.add('flex');
     }else {
         header.classList.remove('navbar-fixed');
+        ToTop.classList.remove('flex');
+        ToTop.classList.add('hidden');
     }
 };
 
@@ -19,3 +24,38 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 })
+
+//Klik di luar hmbgr
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != navMenu){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.toggle('hidden');
+    }
+})
+
+//Dark mode
+
+const darkToggle = document.querySelector('#darktoggle');
+const HTML = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if (darkToggle.checked) {
+        HTML.classList.add('dark');
+        localStorage.theme ='dark';
+
+    } else {
+
+        HTML.classList.remove('dark');
+        localStorage.theme ='light';
+    }
+})
+
+//fixed posisi mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  HTML.classList.add('dark');
+  darkToggle.checked = true;
+} else {
+  HTML.classList.remove('dark');
+  darkToggle.checked= false;
+}
+
